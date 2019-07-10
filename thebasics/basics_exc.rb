dd6b4b4d97486d62157ae7be61313934ee449ddf
Hernando Delgado <hernandodelgado2010@gmail.com>
2019-07-10 09:41:25 -0400

    Added the basics chapter

---
+++ thebasics/basics_exc.rb
@@ -0,0 +1,52 @@
+# Problem 1
+name = "Hernando " + "Delgado"
+puts name
+
+# Problem 2
+puts thousands = 4567 /1000
+puts hundreds = 4567 % 1000 /100
+puts tens = 4567 % 100 /10
+puts ones = 4567 % 10
+
+# Problem 3
+
+movies = {
+  thegodfather: 1975,
+  toystory: 2004,
+  blackswan: 2013,
+  titanic: 2001,
+  et: 1981,
+}
+
+
+puts movies.values
+
+# Problem 4
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+# Problem 5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Problem 6
+
+puts 22.09 * 22.09
+puts 11.09 * 11.09
+puts 4.22 * 4.22
+
+# Problem 7
+
+#SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  #from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+  # signifies theres an open bracket somewhere
