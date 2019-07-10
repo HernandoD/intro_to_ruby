e53946b607fb225d6195d62c415d21bb0493506c
Hernando Delgado <hernandodelgado2010@gmail.com>
2019-07-10 17:37:24 -0400

    variables chapter

---
+++ variables/age.rb
@@ -0,0 +1,9 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "In ten years, you'll be:"
+puts age + 10 
+puts "In twnenty years, you'll be:"
+puts age + 20
+puts "In thirty years, you'll be:"
+puts age + 30
---
+++ variables/name.rb
@@ -0,0 +1,25 @@
+# Problem 1
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "Hi, nice to meet you #{name}!"
+
+# Problem 3
+
+10.times do 
+  puts name
+end
+
+# Problem 4
+
+puts "Whats your first name?"
+f_name = gets.chomp
+puts "Whats your last name?"
+l_name = gets.chomp
+
+puts "Your name is #{f_name} #{l_name}."
+
+# Problem 5
+
+# in the second one, x is not available as it is created inside the do/end block
