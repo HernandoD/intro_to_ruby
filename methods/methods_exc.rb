740f75cfad6c24723b52ad047fc5ccc02160f05f
Hernando Delgado <hernandodelgado2010@gmail.com>
2019-07-11 08:55:30 -0400

    methods chapter

---
+++ methods/methods_exc.rb
@@ -0,0 +1,48 @@
+
+# Problem 1
+def greeting(name)
+  puts "Hi my name is #{name}. Very nice to meet you."
+end
+
+greeting("Hernando")
+
+# Problem 2
+# 2
+# 2 => nil
+# joe => joe
+# four
+# nothing => nil
+
+# Problem 3
+
+def multiply(x,y)
+  puts x * y
+end
+
+multiply(2,3)
+
+# Problem 4
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# Problem 5
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+# returns nil because thats what puts will always return
+
+# Problem 6
+
+# Needed to provide 2 arguments.
+
